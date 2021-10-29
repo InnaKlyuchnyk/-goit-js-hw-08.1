3fda026b21134edc91129c63645dd5ca92f7b756
Inna Klyuchnyk <Innysiaa@ukr.net>
2021-10-30 00:18:12 +0300

    Start task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+let currentTime = 0;
+
+player.on('pause', event => {
+  currentTime = event;
+  console.log('videoplayer-current-time', currentTime);
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
