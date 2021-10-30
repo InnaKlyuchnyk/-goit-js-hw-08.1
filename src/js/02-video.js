9070b5fa0fe3b1f23c4a3870db3616eb48f1b3f6
Inna Klyuchnyk <Innysiaa@ukr.net>
2021-10-31 00:56:19 +0300

    Task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,30 @@
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-let currentTime = 0;
-
-player.on('pause', event => {
-  currentTime = event;
-  console.log('videoplayer-current-time', currentTime);
+player.on('timeupdate', event => {
+  console.log('videoplayer-current-time', event.seconds);
 });
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+
+player
+  .setCurrentTime(0)
+  .then(function (seconds) {
+    console.log(seconds);
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+const formData = {};
+
+populateFormEl();
+
+formEl.addEventListener('submit', onBtnSubmit);
+formEl.addEventListener('input', throttle(onInputTyping, 500));
+
+function onBtnSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const message = formElements.message.value;
+  const formInfomation = {
+    email,
+    message,
+  };
+  console.log(formInfomation);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInputTyping(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateFormEl() {
+  const savedInformation = localStorage.getItem(STORAGE_KEY);
+  const parsedSavedInformation = JSON.parse(savedInformation);
+
+  if (parsedSavedInformation) {
+    inputEl.value = parsedSavedInformation.email;
+    textareaEl.value = parsedSavedInformation.message;
+  }
+}
